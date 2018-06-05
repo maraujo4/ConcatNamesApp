a17a3c0ccecfae1b7c2ddbc20a6d2497838f4e8d
Monica Araujo <priscila_monica78@hotmail.com>
2018-06-05 12:27:34 -0400

    add app center SDK

--- ConcatNamesApp/ConcatNamesApp/App.xaml.cs
+++ ConcatNamesApp/ConcatNamesApp/App.xaml.cs
@@ -1,6 +1,9 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace ConcatNamesApp
@@ -16,8 +19,12 @@ namespace ConcatNamesApp
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+            // Handle when your app starts
+            AppCenter.Start("android=8eea6165-5fea-49a1-a8e0-9937d7e1e54c;" +
+                  "uwp=2e6c5520-b5fb-4aa2-82e8-9307bb2cb8a7;" +
+                  "ios=bbbeed97-939b-41cf-a155-06695ce56e47",
+                  typeof(Analytics), typeof(Crashes));
+        }
 
 		protected override void OnSleep ()
 		{
