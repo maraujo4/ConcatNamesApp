e9d99d7717926cd555d708c4411de9e4d23bebc1
Monica Araujo <priscila_monica78@hotmail.com>
2018-06-12 08:22:01 -0400

    updates...

--- ConcatNamesApp/Tests/UITest/AppInitializer.cs
+++ ConcatNamesApp/Tests/UITest/AppInitializer.cs
@@ -4,19 +4,19 @@ using Xamarin.UITest.Queries;
 
 namespace UITest
 {
-	public class AppInitializer
-	{
-		public static IApp StartApp(Platform platform)
-		{
-			if (platform == Platform.Android)
-			{
-                string apkFilePath = @"C:\github\Source\Repos\ConcatNamesApp\ConcatNamesApp\ConcatNamesApp.Android\bin\Release\com.companyname.ConcatNamesApp - Signed.apk";
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                string apkFilePath = @"C:\github\Source\Repos\ConcatNamesApp\ConcatNamesApp\ConcatNamesApp.Android\bin\Release\com.companyname.ConcatNamesApp-Signed.apk";
                 //string apkFilePath = @"C:\apks\com.companyname.ConcatNamesApp.apk";
                 ConfigureApp.Android.ApkFile(apkFilePath).StartApp();
-                return ConfigureApp.Android.StartApp();                
+                return ConfigureApp.Android.StartApp();
             }
 
-			return ConfigureApp.iOS.StartApp();
-		}
-	}
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
 }
